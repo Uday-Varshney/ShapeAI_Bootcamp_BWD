10954321642db9edd7d0ee2d4936cbdba9e02524
Uday-Varshney <85391692+Uday-Varshney@users.noreply.github.com>
2021-06-05 16:04:06 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Hacking</h1>
+      <p>
+        Google hacking" involves using advanced operators in the Google search
+        engine to locate specific errors of text within search results.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
